21ff4226b75e8101b3771b4635dff25c9f5e699f
Myungho-Kim <myungho96@naver.com>
2023-04-21 15:27:57 +0900

    feat: add backend file
    add backend file

---
+++ backend/mmart/src/main/kotlin/com/ssafy/mmart/MmartApplication.kt
@@ -0,0 +1,11 @@
+package com.ssafy.mmart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MmartApplication
+
+fun main(args: Array<String>) {
+	runApplication<MmartApplication>(*args)
+}
---
+++ backend/mmart/src/test/kotlin/com/ssafy/mmart/MmartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ssafy.mmart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MmartApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
