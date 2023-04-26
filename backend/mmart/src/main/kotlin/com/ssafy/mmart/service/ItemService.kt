6cd8f4cd222786fecf709c0dfa5f04c0aa8c6f98
unknown <hi_cookie@nate.com>
2023-04-26 14:19:02 +0900

    Fix: fix refactoring entity

--- backend/mmart/src/main/kotlin/com/ssafy/mmart/service/ItemService.kt
+++ backend/mmart/src/main/kotlin/com/ssafy/mmart/service/ItemService.kt
@@ -2,35 +2,35 @@ package com.ssafy.mmart.service
 
 import com.querydsl.jpa.impl.JPAQueryFactory
 import com.ssafy.mmart.domain.item.Item
-import com.ssafy.mmart.domain.item.QProduct.product
-import com.ssafy.mmart.repository.ProductRepository
+import com.ssafy.mmart.domain.item.QItem.item
+import com.ssafy.mmart.repository.ItemRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class ProductService @Autowired constructor(
-    val productRepository : ProductRepository,
+class ItemService @Autowired constructor(
+    val itemRepository : ItemRepository,
     val jpaQueryFactory: JPAQueryFactory,
 ) {
     //유저의 구매내역에 따라 검색 부분 리스트는 수정해야함(부가)
-    fun getItem(productIdx: Int, userIdx: Int): Item? {
+    fun getItem(itemIdx: Int, userIdx: Int): Item? {
         return jpaQueryFactory
-            .selectFrom(product)
-            .where(product.productIdx.eq(productIdx))
+            .selectFrom(item)
+            .where(item.itemIdx.eq(itemIdx))
             .fetchOne()
     }
 
     fun getItemByBarcode(barcode: String): Item? {
         return jpaQueryFactory
-            .selectFrom(product)
-            .where(product.barcode.eq(barcode))
+            .selectFrom(item)
+            .where(item.barcode.eq(barcode))
             .fetchOne()
     }
 
     fun getItemByCategory(userIdx: Int, categoryIdx: Int): List<Item?> {
         return jpaQueryFactory
-            .selectFrom(product)
-            .where(product.category.categoryIdx.eq(categoryIdx))
+            .selectFrom(item)
+            .where(item.category.categoryIdx.eq(categoryIdx))
             .fetch()
     }
 }
