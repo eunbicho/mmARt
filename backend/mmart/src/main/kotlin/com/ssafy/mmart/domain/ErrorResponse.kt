44957f7f1304a6ffa6ec235e2009b7b91249b456
Myungho-Kim <myungho96@naver.com>
2023-04-26 17:02:43 +0900

    Feat:commit
    commit

--- backend/mmart/src/main/kotlin/com/ssafy/mmart/domain/ErrorResponse.kt
+++ backend/mmart/src/main/kotlin/com/ssafy/mmart/domain/ErrorResponse.kt
@@ -3,8 +3,8 @@ package com.ssafy.mmart.domain
 import com.ssafy.mmart.exception.ErrorCode
 
 class ErrorResponse(errorCode: ErrorCode, msg:String?) {
-    private val errorCode: String = errorCode.name
-    private val message: String? = msg ?: errorCode.message
+    val errorCode: String = errorCode.name
+    val message: String? = msg ?: errorCode.message
     constructor(errorCode: ErrorCode) : this(errorCode, null)
 
 
