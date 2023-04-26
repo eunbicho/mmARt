95350ec84d4ee793076ec4f4b33d121e30111ba4
unknown <hi_cookie@nate.com>
2023-04-26 11:25:40 +0900

    Feat: finish user API

--- backend/mmart/src/main/kotlin/com/ssafy/mmart/repository/UserRepository.kt
+++ backend/mmart/src/main/kotlin/com/ssafy/mmart/repository/UserRepository.kt
@@ -1,16 +1,13 @@
 package com.ssafy.mmart.repository
 
-import com.querydsl.jpa.impl.JPAQueryFactory
-import com.ssafy.mmart.domain.user.QUser
-import com.ssafy.mmart.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
+import com.ssafy.mmart.domain.user.User
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import javax.annotation.Resource
+import java.util.*
 
 @Repository
 interface UserRepository : JpaRepository<User, Int> {
-
+    fun findByEmail(email: String): User?
 }
 
 //@Repository
