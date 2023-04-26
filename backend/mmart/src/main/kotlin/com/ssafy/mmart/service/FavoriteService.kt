526bfe0dd7226d35f48a502201b6842fd9c40ac6
Myungho-Kim <myungho96@naver.com>
2023-04-26 17:33:35 +0900

    Feat: resolve 6 api
    resolve 6 api

--- backend/mmart/src/main/kotlin/com/ssafy/mmart/service/FavoriteService.kt
+++ backend/mmart/src/main/kotlin/com/ssafy/mmart/service/FavoriteService.kt
@@ -3,12 +3,9 @@ package com.ssafy.mmart.service
 import com.querydsl.jpa.impl.JPAQueryFactory
 import com.ssafy.mmart.domain.favorite.dto.CreateFavoriteReq
 import com.ssafy.mmart.domain.favoriteCategory.Favorite
-import com.ssafy.mmart.domain.favoriteCategory.QFavorite.favorite
-import com.ssafy.mmart.domain.item.Item
 import com.ssafy.mmart.exception.bad_request.BadAccessException
 import com.ssafy.mmart.exception.conflict.FavoriteDuplicateException
-import com.ssafy.mmart.exception.not_found.FavoriteNotFoundException
-import com.ssafy.mmart.exception.not_found.UserNotFoundException
+import com.ssafy.mmart.exception.not_found.*
 import com.ssafy.mmart.repository.CategoryRepository
 import com.ssafy.mmart.repository.FavoriteRepository
 import com.ssafy.mmart.repository.ItemRepository
@@ -28,8 +25,7 @@ class FavoriteService @Autowired constructor(
     fun getFavorites(userIdx: Int): List<Favorite>? {
         //유저가 존재하는지 확인
         userRepository.findById(userIdx).orElseThrow(::UserNotFoundException)
-        val favoriteList = favoriteRepository.findAllByUser_UserIdx(userIdx)
-        return favoriteList
+        return favoriteRepository.findAllByUser_UserIdx(userIdx)
     }
     @Transactional
     fun deleteFavorite(userIdx: Int, favoriteIdx:Int): Favorite? {
@@ -46,10 +42,13 @@ class FavoriteService @Autowired constructor(
     @Transactional
     fun createFavorite(createFavoriteReq: CreateFavoriteReq): Favorite? {
         //중복된 즐겨찾기가 있는지 확인
-        favoriteRepository.findByUser_UserIdxAndItem_ItemIdx(createFavoriteReq.userIdx,createFavoriteReq.itemIdx)?:throw FavoriteDuplicateException()
-        val result = Favorite(user = userRepository.findById(createFavoriteReq.userIdx).get(),
-            category = categoryRepository.findById(createFavoriteReq.categoryIdx).get(),
-            item = itemRepository.findById(createFavoriteReq.itemIdx).get())
+        val fav = favoriteRepository.findByUser_UserIdxAndItem_ItemIdx(createFavoriteReq.userIdx,createFavoriteReq.itemIdx)
+        if (fav != null) {
+            throw FavoriteDuplicateException()
+        }
+        val result = Favorite(user = userRepository.findById(createFavoriteReq.userIdx).orElseThrow(::UserNotFoundException),
+            category = categoryRepository.findById(createFavoriteReq.categoryIdx).orElseThrow(::CategoryNotFoundException),
+            item = itemRepository.findById(createFavoriteReq.itemIdx).orElseThrow(::ItemNotFoundException))
         return favoriteRepository.save(result)
     }
 }
