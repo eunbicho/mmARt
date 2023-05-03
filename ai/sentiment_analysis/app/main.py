29691e7b8e1a51367d3e785dadaad0e606fd1d86
kdoubleh22 <kdoubleh22@naver.com>
2023-05-03 18:07:33 +0900

    Fix: git restore
    Former-commit-id: 2afa4ce6c81732c04fdf1f7e14fab8c8caaaa36e

--- ai/sentiment_analysis/app/main.py
+++ ai/sentiment_analysis/app/main.py
@@ -1,30 +1,9 @@
 from fastapi import FastAPI
-from app.model.model import load_model
-import tensorflow as tf
-from pydantic import BaseModel
-
-
-class Review(BaseModel):
-    review: str
 
 
 app = FastAPI()
 
 
-model = load_model()
-
-
-@app.post("/reviews/sentiment-analysis")
-def post_predict(review: Review):
-    result = tf.sigmoid(model(tf.constant([review.review])))
-
-    score = result.numpy()[0][0]
-
-    prediction=-1
-
-    if(score>=0.5):
-        prediction=1
-    else:
-        prediction=0
-
-    return prediction
+@app.get("/")
+def connect():
+    return "server connection success"
