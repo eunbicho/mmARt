dc0dfca34ef0d25194fe2350bf2817a9b63b7a21
kdoubleh22 <kdoubleh22@naver.com>
2023-05-08 17:16:22 +0900

    Fix: modify matrix with 52 cities
    Former-commit-id: 443c8ff55b305d4193a9238bfcfb3f230a234d1e

--- ai/shortest-path/app/load_distance_matrix.py
+++ ai/shortest-path/app/load_distance_matrix.py
@@ -4,42 +4,42 @@ from app.util import build_distance_matrix
 
 
 def load_distance_matrix(INF):
-    # held-karp test
-    # matrix 생성.
-    distance_matrix = [[INF for j in range(5)] for i in range(5)]
+    # # held-karp test
+    # # matrix 생성.
+    # distance_matrix = [[INF for j in range(5)] for i in range(5)]
 
-    # edge 추가. (양방향)
-    distance_matrix[0][1]=3
-    distance_matrix[1][0]=3
+    # # edge 추가. (양방향)
+    # distance_matrix[0][1]=3
+    # distance_matrix[1][0]=3
 
-    distance_matrix[0][3]=7
-    distance_matrix[3][0]=7
+    # distance_matrix[0][3]=7
+    # distance_matrix[3][0]=7
 
-    distance_matrix[1][2]=2
-    distance_matrix[2][1]=2
+    # distance_matrix[1][2]=2
+    # distance_matrix[2][1]=2
 
-    distance_matrix[2][3]=1
-    distance_matrix[3][2]=1
+    # distance_matrix[2][3]=1
+    # distance_matrix[3][2]=1
 
-    distance_matrix[3][4]=5
-    distance_matrix[4][3]=5
+    # distance_matrix[3][4]=5
+    # distance_matrix[4][3]=5
 
-    # 자기 자신과의 거리는 0
-    for i in range(5):
-        distance_matrix[i][i]=0
+    # # 자기 자신과의 거리는 0
+    # for i in range(5):
+    #     distance_matrix[i][i]=0
 
 
-    # # 2-opt test
-    # # 현재 작업 중인 디렉토리를 확인합니다.
-    # cwd = os.getcwd()
+    # 2-opt test
+    # 현재 작업 중인 디렉토리를 확인합니다.
+    cwd = os.getcwd()
 
-    # # 파일 경로를 지정합니다.
-    # file_path = os.path.join(cwd, 'app/TSP-02-Coordinates.txt')
+    # 파일 경로를 지정합니다.
+    file_path = os.path.join(cwd, 'app/TSP-02-Coordinates.txt')
 
-    # coordinates = pd.read_csv(file_path, sep = '\t')
-    # coordinates = coordinates.values
+    coordinates = pd.read_csv(file_path, sep = '\t')
+    coordinates = coordinates.values
 
-    # distance_matrix = build_distance_matrix(coordinates)
+    distance_matrix = build_distance_matrix(coordinates)
     
 
     # real
