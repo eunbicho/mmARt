0aa7b1cc02c9075cc1c191e4e33a30b62c8be9f9
YJW <ykm9397@naver.com>
2023-05-09 10:20:31 +0900

    FE/Fix: fix error
    Former-commit-id: ce2b63ef599f48abea1e653274c87a71107958d8

--- frontend/mmARt/app/src/main/java/com/example/mmart/GotCart.kt
+++ frontend/mmARt/app/src/main/java/com/example/mmart/GotCart.kt
@@ -1,39 +1,39 @@
 package com.example.mmart
 
+import android.media.Image
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.material.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.semantics.Role.Companion.Image
 import coil.compose.AsyncImage
 
 import kotlinx.coroutines.*
 
 @Composable
-fun GotCart(navController: NavController, userId: Int?){
+fun GotCart(navController: NavController){
 
     val api = APIS.create()
     val coroutineScope = rememberCoroutineScope()
-    var resultCart: CartContent? by remember { mutableStateOf(null) }
+    var result: CartContent? by remember { mutableStateOf(null) }
     var resultCode: String? by remember { mutableStateOf(null) }
-    var resultUser: UserInfo? by remember { mutableStateOf(null) }
-
-    var showQrcode by remember { mutableStateOf(false)}
 
     // 한 번만 실행
     LaunchedEffect(true) {
-        val cartRes = coroutineScope.async { api.gotCartsRead(userId!!) }.await()
-        resultCart = cartRes.result
-        resultCode = cartRes.resultCode
-        val userRes = coroutineScope.async { api.getUser(userId!!) }.await()
-        resultUser = userRes.result
+       val response = coroutineScope.async { api.gotCartsRead(userId!!) }.await()
+        result = response.result
+        resultCode = response.resultCode
     }
 
 
@@ -45,70 +45,30 @@ fun GotCart(navController: NavController, userId: Int?){
             Button(onClick = { navController.navigate("main") }) {
                 Text(text = "메인으로")
             }
-
-            Button(onClick = { showQrcode = true }) {
-                Text(text = "결제하기")
-            }
         }
 
         // result가 null이 아닐 경우만
-        if(resultCart != null){
+        if(result != null){
             if (resultCode == "SUCCESS") {
                 LazyColumn(
                 ){
-                    items(resultCart!!.itemList){
+                    items(result!!.itemList){
                             item ->
                         Row(
                             modifier = Modifier.fillMaxWidth(),
                             horizontalArrangement = Arrangement.SpaceBetween
                         ){
-                            // Text(text = "${item.itemIdx}")
-                            AsyncImage(
-                                model = "https://mmart405.s3.ap-northeast-2.amazonaws.com/${item.thumbnail}",
-                                contentDescription = item.itemName
-                            )
-                            Text(text = item.itemName)
-                            Text(text = "수량 : ${item.quantity}")
-                            Text(text = "${item.price}원")
+//                            Text(text = "${item.itemIdx}")
+
                         }
                     }
                 }
-                    Text(text = "합계 : ${resultCart!!.total}원")
 
             } else {
-                // Text(resultCart!!.message)
+//                Text(result!!.message)
             }
         }
 
 
-    }
-
-    if (showQrcode) {
-        Dialog(
-            onDismissRequest = { showQrcode = false },
-            content = {
-                Surface (
-                    modifier = Modifier.fillMaxWidth(),
-                    elevation = 8.dp
-                ){
-                    Column(
-                        modifier = Modifier.padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-                        Text("아래 QR코드를 키오스크에 인식해주세요.")
-                        AsyncImage(
-                            model = "https://mmart405.s3.ap-northeast-2.amazonaws.com/${resultUser?.qrcode}",
-                            modifier = Modifier.fillMaxWidth(),
-                            contentDescription = resultUser?.name
-                        )
-                        Button(onClick = {
-                            showQrcode = false
-                        }) {
-                            Text("닫기")
-                        }
-                    }
-                }
-            }
-        )
     }
 }
