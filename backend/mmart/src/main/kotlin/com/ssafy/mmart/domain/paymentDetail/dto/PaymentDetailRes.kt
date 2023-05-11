b72596e569c67df14e105942345decdd2dabdd84
Myungho-Kim <myungho96@naver.com>
2023-05-11 12:33:41 +0900

    Feat : update paymentDetail
    update paymentDetail
    
    Former-commit-id: fcdb6a1b4dec0239ca539900adc770b7eecae31e

--- backend/mmart/src/main/kotlin/com/ssafy/mmart/domain/paymentDetail/dto/PaymentDetailRes.kt
+++ backend/mmart/src/main/kotlin/com/ssafy/mmart/domain/paymentDetail/dto/PaymentDetailRes.kt
@@ -1,17 +1,8 @@
 package com.ssafy.mmart.domain.paymentDetail.dto
 
-import com.ssafy.mmart.domain.item.Item
 import com.ssafy.mmart.domain.item.dto.GetItemRes
-import com.ssafy.mmart.domain.payment.Payment
-import com.ssafy.mmart.domain.paymentDetail.PaymentDetail
-import com.ssafy.mmart.domain.review.Review
-import com.ssafy.mmart.domain.user.User
-import com.ssafy.mmart.domain.user.dto.UserRes
-import org.hibernate.annotations.OnDelete
-import org.hibernate.annotations.OnDeleteAction
-import javax.persistence.*
 
-data class PaytmentDetailRes (
+data class PaymentDetailRes (
     val paymentDetailIdx: Int,
     val quantity: Int,
     val discount: Int,
