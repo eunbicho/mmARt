398ba86bd2b3928c11041c6e166d66fe4f6ec6c8
YJW <ykm9397@naver.com>
2023-05-16 10:01:26 +0900

    [S08P31A405-385] FE/Feat: add recentItems & frequentItems, design category items & searched items
    메인페이지 최근 구매, 자주 구매 아이템추가, 카테고리 별/검색 결과 아이템들 디자인 & 하단 버튼 추가
    메인/카테고리별아이템/검색결과아이템/상품상세 로딩창 추가
    
    
    Former-commit-id: bdc82b49356118952b12bfdc83c2c7ec278421c2

--- frontend/mmARt/app/src/main/java/com/example/mmart/Tools.kt
+++ frontend/mmARt/app/src/main/java/com/example/mmart/Tools.kt
@@ -6,31 +6,47 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 //import androidx.compose.material.OutlinedTextField
 //import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import com.example.mmart.ui.theme.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import okhttp3.internal.wait
 
 // 검색바
 @OptIn(ExperimentalComposeUiApi::class)
@@ -223,18 +239,213 @@ fun blankView(msg: String) {
 
 @Composable
 fun loadingView(){
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        contentAlignment = Alignment.Center,
     ) {
         Text("로딩")
     }
 }
 
+@Composable
+fun items(navController:NavController, items: List<ItemInfo>){
+    val api = APIS.create()
+    val coroutineScope = rememberCoroutineScope()
+    val listState = rememberLazyGridState()
+    
+    var isLoading: Boolean by remember { mutableStateOf(true) }
+
+    // 이미지 로딩 중일 때
+    if(isLoading){
+        loadingView()
+    }
+
+    Box(modifier = Modifier.fillMaxSize().padding(vertical = 10.dp)) {
+        LazyVerticalGrid(GridCells.Fixed(2), state=listState) {
+            items(items) { item ->
+                // 수량
+                var quantity: Int by remember { mutableStateOf(1) }
+                // 장볼구니 모달
+                var goGetCart: Boolean by remember { mutableStateOf(false) }
+                // 장볼구니 추가
+                fun addGetCart(){
+                    val body = mapOf(
+                        "itemIdx" to item.itemIdx,
+                        "quantity" to quantity,
+                        "userIdx" to userId
+                    )
+                    coroutineScope.launch{
+                        try {
+                            val resultCode = api.addGetCart(body).resultCode
+                            if(resultCode == "SUCCESS"){
+                                goGetCart = true
+                            }
+                        } catch (e: Exception){
+                            println("장볼구니 추가 에러------------------")
+                            e.printStackTrace()
+                            println("---------------------------------")
+                        }
+                    }
+                }
+
+
+                Column(
+                    modifier = Modifier.padding(10.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp)
+                            .clickable { navController.navigate("item/${item.itemIdx}") }
+                    ){
+                        AsyncImage(
+                            model = "https://mmart405.s3.ap-northeast-2.amazonaws.com/${item.thumbnail.replace("_thumb", "")}",
+                            contentDescription = "상품 썸네일",
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .aspectRatio(1f)
+                                .padding(5.dp),
+                            onSuccess = {isLoading = false}
+                        )
+
+                        Text(text = item.itemName, maxLines = 1, overflow = TextOverflow.Ellipsis, fontSize = 20.sp)
+
+                        if(item.isCoupon){
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text("${item.couponPrice}원", fontSize = 20.sp, modifier = Modifier.padding(5.dp))
+                                Text("${item.price}", textDecoration = TextDecoration.LineThrough, color = Color.LightGray, fontWeight = FontWeight.Light, fontSize = 15.sp)
+                            }
+                        } else{
+                            Text(text = "${item.price}원", fontSize = 20.sp)
+                        }
+                    }
+                    // 수량, 장볼구니 추가 버튼
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        IconButton(
+                            onClick = { quantity-- },
+                            enabled = 1 < quantity
+                        ) {
+                            Icon(Icons.Default.KeyboardArrowLeft, contentDescription = "수량 감소")
+                        }
+                        Text(quantity.toString())
+                        IconButton(
+                            onClick = { quantity++ },
+                            enabled = quantity < item.inventory
+                        ) {
+                            Icon(Icons.Default.KeyboardArrowRight, contentDescription = "수량 증가")
+                        }
+
+                        Image(painter = painterResource(R.drawable.getcart), contentDescription = "장볼구니 추가",
+                            modifier = Modifier
+                                .size(40.dp)
+                                .padding(end = 20.dp)
+                                .weight(1f)
+                                .wrapContentWidth(align = Alignment.End)
+                                .clickable { addGetCart() }
+                        )
+                    }
+                }
+
+                // 장볼구니 모달
+                if (goGetCart){
+                    Dialog(
+                        onDismissRequest = { goGetCart = false }
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .border(
+                                    width = 3.dp,
+                                    color = Color.Black,
+                                    shape = RoundedCornerShape(40.dp)
+                                )
+                                .shadow(10.dp, shape = RoundedCornerShape(40.dp))
+                                .background(color = Color.White, shape = RoundedCornerShape(40.dp))
+                        ){
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text("상품이 장볼구니에\n추가되었습니다", modifier = Modifier.padding(horizontal = 30.dp, vertical = 20.dp), fontSize = 20.sp, textAlign = TextAlign.Center)
+                                Row(
+                                    horizontalArrangement = Arrangement.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(horizontal = 50.dp)
+                                ) {
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        modifier = Modifier
+                                            .padding(10.dp)
+                                            .clickable { goGetCart = false }
+                                    ) {
+                                        Image(painter = painterResource(R.drawable.previous), contentDescription = "이전으로", Modifier.size(80.dp))
+                                        Text("이전으로", Modifier.padding(5.dp))
+                                    }
+                                    Column(
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                        modifier = Modifier
+                                            .padding(10.dp)
+                                            .clickable { navController.navigate("getCart") }
+                                    ) {
+                                        Image(painter = painterResource(R.drawable.getcart), contentDescription = "장볼구니로", Modifier.size(80.dp))
+                                        Text("장볼구니로", Modifier.padding(5.dp))
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Row(
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxHeight()
+        ) {
+            FloatingActionButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.animateScrollToItem(index = 0)
+                    }},
+                backgroundColor = Light_gray,
+                modifier = Modifier
+                    .sizeIn(60.dp, 60.dp, 80.dp, 80.dp),
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.top),
+                    contentDescription = "TOP",
+                )
+            }
+        }
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
-fun ToolPreview(){
-    val navController = rememberNavController()
-    topBar(navController = navController, "상세보기")
+fun toolPreview(){
+    Image(
+        painter = painterResource(R.drawable.bg),
+        modifier = Modifier.fillMaxSize(),
+        contentDescription = "배경",
+        contentScale = ContentScale.FillBounds
+    )
+    Box(){
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text(text = "aaaaaaaaaaa")
+            Text("뭐야아앙아ㅏ")
+            loadingView()
+        }
+    }
+
 }
+
