4eae9c87ebda57614650c8f299f959c7dee1b168
권택윤 <thomazkwon@gmail.com>
2023-05-16 21:53:08 +0900

    fix: Review.kt layout
    Former-commit-id: 547dead5047c1b594bab01c16136390817c0a885

--- frontend/mmARt/app/src/main/java/com/example/mmart/Review.kt
+++ frontend/mmARt/app/src/main/java/com/example/mmart/Review.kt
@@ -2,6 +2,7 @@ package com.example.mmart
 
 import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -16,6 +17,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -75,7 +77,34 @@ fun Review(navController: NavController) {
         if (reviews != null) {
             // 작성한 리뷰가 없을 때
             if (reviews!!.isEmpty()) {
-                blankView("작성한 리뷰가 없습니다.")
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    blankView("작성한 리뷰가 없습니다.")
+                    Row(
+                        modifier = Modifier.align(Alignment.BottomCenter),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .clickable { navController.navigate("main") }
+                        ) {
+                            Image(painter = painterResource(R.drawable.main), contentDescription = "홈으로", Modifier.size(80.dp))
+                            Text("홈으로", Modifier.padding(5.dp))
+                        }
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .clickable { navController.navigate("payment") }
+                        ) {
+                            Image(painter = painterResource(R.drawable.payment), contentDescription = "결제내역으로", Modifier.size(80.dp))
+                            Text("결제내역으로", Modifier.padding(5.dp))
+                        }
+                    }
+                }
 
                 // 작성한 리뷰가 있을 때
             } else {
